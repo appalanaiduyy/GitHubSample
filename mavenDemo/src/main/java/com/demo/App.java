4c85a3baf2aa9d310fce8fd214f36ffb4ad4bb91
appalanaiduyy <appalanaidu223@gmail.com>
2018-09-21 10:28:43 +0530

    Sencond change

--- mavenDemo/src/main/java/com/demo/App.java
+++ mavenDemo/src/main/java/com/demo/App.java
@@ -3,6 +3,7 @@ package com.demo;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Sample");
+		System.out.println("Sampleadd");
 	}
 public String sample() {
 	return "Sample";
